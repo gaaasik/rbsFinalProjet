faa5db6cf0c64c34eac9deca25257dd4722dd433
PavelR <repening.pavel@gmail.com>
2020-06-19 18:06:31 +0400

    too
    Signed-off-by: PavelR <repening.pavel@gmail.com>

--- main.js
+++ main.js
@@ -2,52 +2,84 @@ let asesTable = [
     {number: 1, name: "Собеседование раз", state: "Создано"}
 ]
 let viewIS = true;
+
 function addActivity() {
+
     var popup = webix.ui({
-                        view:"popup",
-                        id:"pop1",
-                        height:450,
-                        width:1000,modal:true,
-                        position: "absolute",
-                        top: "50%",
-                        left: "50%",
-                        margin_right: "-50%",
-                        body:{
-
-                                cols:[{ view:"template"}],
-                                view:"button",value:"Закрыть", css:"closePop", click:function(){
-                                    $$("pop1").hide();
-                                    console.log("Нажали")
-                                }}
-
-
-
-}).show();
+        view: "popup",
+        id: "pop1",
+        height: 450,
+        width: 1000,
+        modal: true,
+        position: "absolute",
+        top: "50%",
+        left: "50%",
+        margin_right: "-50%",
+        body: {
+            rows: [{
+                cols: [{view: "template", width: 100, type: "header", template: "Номер"},
+                    {view: "template", width: 150, type: "header", template: "Название"},
+                    {view: "template", width: 150, type: "header", template: "Дата"},
+                    {view: "template", width: 150, type: "header", template: "Состояние"},
+                    {view: "template", type: "header", template: "Результат собеседования      "}
+                ]
+            }
+            // ,
+            //
+            //
+            //     {cols: [{
+            //             view: "button", value: "Закрыть", css: "closePop", click: function () {
+            //                 $$("pop1").hide();
+            //                 console.log("Нажали")
+            //             }
+            //         }]
+            //     }
+        ]}}).show();
     console.log("pop")
 
 }
-function deleteActivity(){
+
+function deleteActivity() {
     console.log("dellllll")
 }
+
 webix.ready(function () {
     webix.ui({
             rows: [{
                 view: "toolbar", id: "myToolbar",
                 cols: [
                     {view: "template", type: "header", height: 100, template: "шапка "},
-                    {view:"template",id:"delBtn" , hidden:true , template: "Удалить Мероприятие", css:"delActivityClass", width:100,
+                    {
+                        view: "template",
+                        id: "delBtn",
+                        hidden: true,
+                        template: "Удалить Мероприятие",
+                        css: "delActivityClass",
+                        width: 100,
 
-                        onClick:{"delActivityClass":function () {
+                        onClick: {
+                            "delActivityClass": function () {
                                 deleteActivity();
 
-                                }}}
+                            }
+                        }
+                    }
 
-                    ,{view:"template",select:"view", width:150,template: "Добавить мероприятие",css:"addActivityClass",id:"addActivity",
-                        onClick:{"addActivityClass":function () {
-                            console.log("adddddddddddd")
+                    , {
+                        view: "template",
+                        select: "view",
+                        width: 150,
+                        template: "Добавить мероприятие",
+                        css: "addActivityClass",
+                        id: "addActivity",
+                        onClick: {
+                            "addActivityClass": function () {
+                                console.log("adddddddddddd")
                                 addActivity();
 
-                            } }}
+                            }
+                        }
+                    }
 
                 ]
 
@@ -59,29 +91,33 @@ webix.ready(function () {
                             id: "asesTable",
                             select: "row",
                             data: asesTable,
-                            width:430,
+                            width: 430,
 
 
                             columns: [{id: "number", header: "№", width: 30},
                                 {id: "name", width: 200, header: "Название мероприятия"},
-                                {id: "state",width: 200, header: "Статус"},
+                                {id: "state", width: 200, header: "Статус"},
 
-                            ] ,
-                            on: { onitemclick: function(){
+                            ],
+                            on: {
+                                onitemclick: function () {
                                     console.log("clickrow")
 
                                     let delButn = $$("delBtn");
 
                                     delButn.show();
 
-                                }},
+                                }
+                            },
                         }, {view: "resizer"},
-                            {cols:[{
-                                view:"datatable",
-                                    id:"resultPath",
-                                    columns: [{header:"Результат собеседования"}]
+                            {
+                                cols: [{
+                                    view: "datatable",
+                                    id: "resultPath",
+                                    columns: [{header: "Результат собеседования"}]
 
-                                }]}]
+                                }]
+                            }]
                     }]
                 }
 
